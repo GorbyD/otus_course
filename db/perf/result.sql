3422934e796d273cb64838b4f2025476782177de
Denis Gorbachev <gorbyd1987@gmail.com>
2026-07-28 16:59:10 +0300

    hw12

--- db/perf/result.sql
+++ db/perf/result.sql
@@ -1,11 +1,36 @@
--- сброс
-\ir 1_reset.sql
+-- docker compose exec -T postgres psql -U {username} -d {database} -f  /db/perf/result.sql
 
--- создание таблиц
+-- 10K --
+-- \ir 1_reset.sql
+-- \ir ../schema.sql
+-- \ir ../movie_attr.sql
+--
+-- -- генерация данных
+-- \ir 2_generate_10k.sql
+-- \ir generate_body.sql
+--
+-- -- снятие плана
+-- \set stage_dir /db/perf/results/10k
+-- \ir explain.sql
+
+
+-- 10M --
+\ir 1_reset.sql
 \ir ../schema.sql
 \ir ../movie_attr.sql
 
 -- генерация данных
-\ir 2_generate_10k.sql
--- \ir 3_generate_10m.sql
+\ir 3_generate_10m.sql
 \ir generate_body.sql
+
+-- снятие плана
+\set stage_dir /db/perf/results/10m
+\ir explain.sql
+
+
+-- 10M + индексы --
+-- \ir 4_indexes.sql
+--
+-- -- снятие плана
+-- \set stage_dir /db/perf/results/10m_optimized
+-- \ir explain.sql
